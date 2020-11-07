3e1cd93c763a02e99661abfc6ead266642328452
Vladimir Mazhirin <vladimirm85@gmail.com>
2020-11-07 16:22:16 +0200

    feat: add now-template-card

--- card-list/src/x-552800-card-list/index.js
+++ card-list/src/x-552800-card-list/index.js
@@ -1,10 +1,31 @@
 import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
+import '@servicenow/now-template-card'
 import styles from './styles.scss';
 
 const view = (state, {updateState}) => {
 	return (
-		<div></div>
+		<div className="wrapper">
+			<now-template-card-assist
+				tagline={{ icon: "tree-view-long-outline", label: "Process" }}
+				actions={[
+					{ id: "share", label: "Copy URL" },
+					{ id: "close", label: "Mark Complete" },
+				]}
+				heading={{
+					label: "Submit attachment to malware sandbox and review the results",
+				}}
+				content={[
+					{ label: "State", value: { type: "string", value: "Closed" } },
+					{ label: "Assigned", value: { type: "string", value: "Carla S" } },
+					{ label: "Priority", value: { type: "string", value: "Low" } },
+					{ label: "SLA", value: { type: "string", value: "No SLA Set" } },
+				]}
+				contentItemMinWidth="300"
+				footerContent={{ label: "Updated", value: "2019-01-15 08:41:09" }}
+				configAria={{}}
+			></now-template-card-assist>
+		</div>
 	);
 };
 
