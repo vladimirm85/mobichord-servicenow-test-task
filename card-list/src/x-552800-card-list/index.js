dcbf7194f88d745eaf933b3e5ec847a442b0bcb2
Vladimir Mazhirin <vladimirm85@gmail.com>
2020-11-07 16:30:37 +0200

    feat: add generate 4 cards with mock data

--- card-list/src/x-552800-card-list/index.js
+++ card-list/src/x-552800-card-list/index.js
@@ -2,29 +2,42 @@ import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import '@servicenow/now-template-card'
 import styles from './styles.scss';
+import {cardsData} from './cardsData'
 
 const view = (state, {updateState}) => {
+
+  const cards = cardsData.map((cardData) => (
+    <now-template-card-assist
+      className={'card'}
+      tagline={{ icon: "tree-view-long-outline", label: "Incident" }}
+      actions={[
+        { id: "share", label: "Copy URL" },
+        { id: "close", label: "Mark Complete" },
+      ]}
+      heading={{
+        label: cardData.headingLabel,
+      }}
+      content={[
+        { label: "Number", value: { type: "string", value: cardData.number } },
+        { label: "State", value: { type: "string", value: cardData.state } },
+        {
+          label: "Assignment Group",
+          value: { type: "string", value: cardData.assignmentGroup },
+        },
+        {
+          label: "Assigned To",
+          value: { type: "string", value: cardData.assignedTo },
+        },
+      ]}
+      footerContent={{ label: "Updated", value: cardData.updated }}
+      configAria={{}}
+      contentItemMinWidth="300"
+    ></now-template-card-assist>
+  ));
+
 	return (
 		<div className="wrapper">
-			<now-template-card-assist
-				tagline={{ icon: "tree-view-long-outline", label: "Process" }}
-				actions={[
-					{ id: "share", label: "Copy URL" },
-					{ id: "close", label: "Mark Complete" },
-				]}
-				heading={{
-					label: "Submit attachment to malware sandbox and review the results",
-				}}
-				content={[
-					{ label: "State", value: { type: "string", value: "Closed" } },
-					{ label: "Assigned", value: { type: "string", value: "Carla S" } },
-					{ label: "Priority", value: { type: "string", value: "Low" } },
-					{ label: "SLA", value: { type: "string", value: "No SLA Set" } },
-				]}
-				contentItemMinWidth="300"
-				footerContent={{ label: "Updated", value: "2019-01-15 08:41:09" }}
-				configAria={{}}
-			></now-template-card-assist>
+      {cards}
 		</div>
 	);
 };
