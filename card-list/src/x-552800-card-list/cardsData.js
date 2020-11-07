119870900dc9810cb716ef9c733ee7941e2b9759
Vladimir Mazhirin <vladimirm85@gmail.com>
2020-11-07 16:26:04 +0200

    feat: add cards mock data

---
+++ card-list/src/x-552800-card-list/cardsData.js
@@ -0,0 +1,34 @@
+export const cardsData = [
+	{
+		headingLabel: "my PDF docs are all locked from editing",
+		number: "INC0000038",
+		state: "Closed",
+		assignmentGroup: "Service Desk",
+		assignedTo: "Luke Wilson",
+		updated: "2020-05-08 17:36:44",
+	},
+	{
+		headingLabel: "Printer in my office is out of toner",
+		number: "INC0000008",
+		state: "Closed",
+		assignmentGroup: "Hardware",
+		assignedTo: "ITIL User",
+		updated: "2020-05-09 16:08:39",
+	},
+	{
+		headingLabel: "Hangs when trying to print VISIO document",
+		number: "INC0000006",
+		state: "Closed",
+		assignmentGroup: "Software",
+		assignedTo: "Howard Johnson",
+		updated: "2020-05-01 16:08:05",
+	},
+	{
+		headingLabel: "Can't read email",
+		number: "INC0000001",
+		state: "Closed",
+		assignmentGroup: "Service Desk",
+		assignedTo: "Charlie Whithers",
+		updated: "2020-05-01 16:09:51",
+	},
+];
