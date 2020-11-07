ef4a583de3c59476f3a0ed3200ec2b3dd62972f4
Vladimir Mazhirin <vladimirm85@gmail.com>
2020-11-07 19:03:06 +0200

    feat: add HttpEffect

--- incident-list/src/x-552800-incident-list/index.js
+++ incident-list/src/x-552800-incident-list/index.js
@@ -1,8 +1,20 @@
-import {createCustomElement} from '@servicenow/ui-core';
-import snabbdom from '@servicenow/ui-renderer-snabbdom';
-import styles from './styles.scss';
+import { createCustomElement, actionTypes } from "@servicenow/ui-core";
+import snabbdom from "@servicenow/ui-renderer-snabbdom";
+import "@servicenow/now-template-card";
+import styles from "./styles.scss";
+import { createHttpEffect } from "@servicenow/ui-effect-http";
+const { COMPONENT_BOOTSTRAPPED } = actionTypes;
 
-const view = (state, {updateState}) => {
+const INCIDENTS_FETCH_SUCCESS = "INCIDENTS_FETCH_SUCCESS";
+const fetchIncidentsEffect = createHttpEffect(
+  "api/now/table/incident?sysparm_display_value=true",
+  {
+    successActionType: INCIDENTS_FETCH_SUCCESS,
+  }
+);
+
+const view = ({ incidents }) => {
+	console.log(incidents);
 	return (
 		<div></div>
 	);
@@ -11,5 +23,24 @@ const view = (state, {updateState}) => {
 createCustomElement('x-552800-incident-list', {
 	renderer: {type: snabbdom},
 	view,
-	styles
-});
+	styles,
+	initialState: {
+	  incidents: [],
+	},
+	actionHandlers: {
+	  [COMPONENT_BOOTSTRAPPED]: {
+		effect({ dispatch }) {
+		  dispatch("INCIDENTS_FETCHED");
+		},
+	  },
+	  INCIDENTS_FETCHED: fetchIncidentsEffect,
+	  [INCIDENTS_FETCH_SUCCESS]: ({ action, updateState }) => {
+		updateState({
+		  path: "incidents",
+		  value: action.payload.result,
+		  operation: "concat",
+		});
+	  },
+	},
+  });
+  
