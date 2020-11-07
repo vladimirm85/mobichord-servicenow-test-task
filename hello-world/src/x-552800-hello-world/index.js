0a18ea6e36a972a415ea29cdab5d0cc5507cd5ca
Vladimir Mazhirin <vladimirm85@gmail.com>
2020-11-07 15:23:51 +0200

    feat: add now-heading component

--- hello-world/src/x-552800-hello-world/index.js
+++ hello-world/src/x-552800-hello-world/index.js
@@ -1,10 +1,13 @@
 import {createCustomElement} from '@servicenow/ui-core';
+import '@servicenow/now-heading';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 
 const view = (state, {updateState}) => {
 	return (
-		<now-heading label="Hello world!" level="1" variant="header-primary"></now-heading>
+		<div className="wrap">
+			<now-heading label="Hello world!" level="1" variant="header-primary"></now-heading>
+		</div>
 	);
 };
 
